bd1e5d0a85b71069e1d35c46acd20f1f51d25c4b
KSL <kaungsatlinn77@gmail.com>
2025-02-16 21:42:36 +0630

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,71 +3,86 @@
 
 Dictionary<string, char> charMap = new()
 {
-    {"1", 'a'},
-    {"11", 'b'},
-    {"111", 'c'},
-    {"2",'d'},
-    {"22",'e'},
-    {"222", 'f' },
-    {"3",'g'},
-    {"33",'h'},
-    {"333", 'i' },
-    {"4",'j'},
-    {"44",'k'},
-    {"444", 'l' },
-    {"5",'m'},
-    {"55",'n'},
-    {"555", 'o' },
-    {"6",'p'},
-    {"66",'q'},
-    {"666", 'r' },
-    {"6666", 's' },
-    {"7",'t'},
-    {"77",'u'},
-    {"777", 'v' },
-    {"8",'w'},
-    {"88",'x'},
-    {"888", 'y' },
-    {"8888", 'z' },
-    {"9",' ' }
+    {"1", '&'},
+    {"11", '\''},
+    {"111", '('},
+    {"2",'a'},
+    {"22",'b'},
+    {"222", 'c' },
+    {"3",'d'},
+    {"33",'e'},
+    {"333", 'f' },
+    {"4",'g'},
+    {"44",'h'},
+    {"444", 'i' },
+    {"5",'j'},
+    {"55",'k'},
+    {"555", 'l' },
+    {"6",'m'},
+    {"66",'n'},
+    {"666", 'o' },
+    {"7",'p'},
+    {"77",'q'},
+    {"777", 'r' },
+    {"7777", 's' },
+    {"8",'t'},
+    {"88",'u'},
+    {"888", 'v' },
+    {"9",'w' },
+    {"99",'x' },
+    {"999",'y' },
+    {"9999",'z' },
+    {"0",' ' },
+    {"#",'#' },
+    {"*",'*' }
 };
 
-Operation(charMap);
-
+OperationSec(charMap);
 
 static void Operation(Dictionary<string, char> charMap)
 {
     Console.WriteLine("Write Value & Enter to see result!");
     string input = Console.ReadLine();
     string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-    string result = "";
+    string result = string.Empty;
     foreach (string word in words)
-    {
+    {   
         string currentChar = string.Empty;
         string lastChar = string.Empty;
-        string tempVal = string.Empty;
-        for (int i = 0; i < word.Length; i++) {
-            currentChar = word.ElementAt(i).ToString();
-            if (i != 0 && currentChar != lastChar)
-            {
-                if (!charMap.ContainsKey(tempVal) && currentChar != "#")
-                {
-                    result = "Invalid Character!";
-                    break;
-                }
-                result += charMap[tempVal];
-                tempVal = currentChar;
-                if (i == word.Length - 1 && currentChar != "#") {
-                    result += charMap[tempVal];
-                }
+        string tempStr = string.Empty;
+        for (int i = 0; i < word.Length; i++)
+        {
+            currentChar = word[i].ToString();
+            if (!charMap.ContainsKey(currentChar)) {
+                result = "Invalid Character!";
+                break;
             }
-            else
+            if (i == 0)
             {
-                tempVal += currentChar;
-                if (i == word.Length - 1 && currentChar != "#")
-                {
-                    result += charMap[tempVal];
-                }
+                tempStr = currentChar;
+                lastChar = currentChar;
+                continue;
+            }
+            if (currentChar == lastChar)
+            {
+                tempStr += currentChar;
+                lastChar = currentChar;
+                continue;// Without #, loop will stop here and the last char won't show.
+            }
+            if (tempStr != string.Empty)
+            {
+                result += charMap[tempStr].ToString();
+                tempStr = currentChar;
+            }
+            if (i == word.Length - 1 && currentChar != "#" && currentChar != "*") {
+                result += charMap[tempStr].ToString();
+            }
+            if (currentChar == "*")
+            {
+                result = result != string.Empty ? result.Remove(result.Length - 1) : string.Empty;
+                tempStr = string.Empty;
+                lastChar = string.Empty;
+                continue;
             }
             lastChar = currentChar;
         }
@@ -83,3 +98,56 @@ static void Operation(Dictionary<string, char> charMap)
     }
 }
 
+static void OperationSec(Dictionary<string, char> charMap)
+{
+    Console.WriteLine("Write Value & Enter to see result!");
+    string input = Console.ReadLine();
+    string result = string.Empty;
+    string currentChar = string.Empty;
+    string lastChar = string.Empty;
+    string tempStr = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        currentChar = input[i].ToString();
+        if (!charMap.ContainsKey(currentChar) && !string.IsNullOrWhiteSpace(currentChar))
+        {
+            result = "Invalid Character!";
+            break;
+        }
+        if (i == 0 || string.IsNullOrWhiteSpace(lastChar))
+        {
+            tempStr = currentChar;
+            lastChar = currentChar;
+            continue;
+        }
+        if (currentChar == lastChar && lastChar != "*")
+        {
+            tempStr += currentChar;
+            lastChar = currentChar;
+            continue;// Without #, loop will stop here and the last char won't show.
+        }
+        if (!string.IsNullOrWhiteSpace(tempStr))
+        {
+            result += charMap[tempStr].ToString();
+            tempStr = string.IsNullOrWhiteSpace(currentChar) ? string.Empty : currentChar;
+        }
+        if (currentChar == "*")
+        {
+            result = result != string.Empty ? result.Remove(result.Length - 1) : string.Empty;
+            tempStr = string.Empty;
+        }
+        if (lastChar == "*" && currentChar != "*") {
+            tempStr = currentChar;
+        }
+        lastChar = currentChar;
+    }
+    Console.WriteLine("Result: " + result.ToUpper());
+    Console.WriteLine();
+    Console.WriteLine("Press Any Key to Start or 'e' to Quit: ");
+    Console.WriteLine();
+    var key = Console.ReadKey();
+    if (key.KeyChar != 'e' && key.KeyChar != 'E')
+    {
+        OperationSec(charMap);
+    }
+}
